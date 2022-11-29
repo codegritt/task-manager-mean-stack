c342076eee38c8769061b2d42cbb1545986208b0
gokul <gokulchandan18@gmail.com>
2022-11-29 11:37:29 +0530

    commit

--- frontend/api/db/mongoose.js
+++ frontend/api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb+srv://gokul:gokul1234@eventsdb.d45qs.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
